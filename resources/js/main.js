1ea2ff0fdd23b30e76f670fe00a1cf3cb02220c2
Victor Trofimciuc <victortrofimciuc@gmail.com>
2017-11-17 17:23:05 -0600

    style sticky header; add body background

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,18 +1,18 @@
-$(function () {
-
-    createSticky($("nav"));
-
-});
-
-function createSticky(sticky) {
-
-    if (typeof sticky !== "undefined") {
-
-        var pos = sticky.offset().top,
-            win = $(window);
-
-        win.scroll(function () {
-            win.scrollTop() >= pos ? sticky.addClass("sticky") : sticky.removeClass("sticky");
-        });
-    }
-}
+// $(function () {
+//
+//     createSticky($("nav"));
+//
+// });
+//
+// function createSticky(sticky) {
+//
+//     if (typeof sticky !== "undefined") {
+//
+//         var pos = sticky.offset().top,
+//             win = $(window);
+//
+//         win.scroll(function () {
+//             win.scrollTop() >= pos ? sticky.addClass("sticky") : sticky.removeClass("sticky");
+//         });
+//     }
+// }
